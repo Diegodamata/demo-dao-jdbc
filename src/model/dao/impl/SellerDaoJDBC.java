a2ec90db2e5a355dcae4fa91d8103416c78413a8
Diego da Mata <Diegodamata>
2024-08-25 19:45:55 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -109,6 +112,72 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement ps = null; //serve para uma execução (comando) especifica no DB
+		ResultSet rs = null; //ira receber o resultado do statment em formato de tabela
+		//a conexão com o DB não posso fazer aqui pois outras classes tambem pode usar essa conexão, 
+		
+		try {
+			
+			ps = conn.prepareStatement(
+					" SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			ps.setInt(1, department.getId()); //configurando o "?"
+			
+			rs = ps.executeQuery(); //pedindo para executar a operação
+			
+			//e como são varios valores que pode retornar do banco então criei uma lista
+			List<Seller> list = new ArrayList<Seller>();
+			
+			//criei uma estrutura map vazia, ela não permite repetições de chave
+			Map<Integer, Department> map = new HashMap<>();
+			
+			//meu resultado pode ser 0 ou mais valor então uso a estrutura while para percorrer
+			//e adicionar todos os valores na minha lista
+			while(rs.next()) { 
+				
+				//porem dessa forma é uma aplicação ruim, pois cada vendedor vai estar
+				//apontando para departamentos direfentes, sendo que é o mesmo departamento
+				//Department dep = instantiateDepartment(rs);
+				//Seller seller = instantiateSeller(rs, dep);
+				//list.add(seller);
+				
+				//forma correta
+				
+				//crio uma variavel do tipo department chamo o map e verifico se no meu map ja contem o id que esta na coluna DepatmentId
+				//se não existir ira me retornal null
+				//ai sim eu instancio o meu objeto
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) { //se o dep for null quer dizer que não conten esse valor no meu map
+					
+					dep = instantiateDepartment(rs); //se o dep não estiver vazio, estao atribuo o resultado da instancia do department
+					map.put(rs.getInt("DepartmentId"), dep); //entao eu adiciono no meu map o id e o department
+					//porem se estiver valor repedido o mep não aceitara, dessa forma os sellers estara apontando pro mesmo department
+				}
+				
+				Seller seller = instantiateSeller(rs, dep);
+				list.add(seller);
+				
+			}
+			
+			return list; //se não existir uma linha peço para retornar null
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(ps);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
